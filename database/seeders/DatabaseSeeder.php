a938e3670e00cbb980362c12d8f56b5f1ec99f2e
WGuillenDev <wilberthguillen.ing@gmail.com>
2026-08-13 10:02:52 -0600

    feat(database): seed Estudiante and Coordinadora de Docencia test roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Infrastructure\Persistence\Eloquent\Students\Models\StudentModel;
 use App\Models\Permission;
 use App\Models\Role;
 use App\Models\User;
@@ -45,5 +46,34 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $adminUser->roles()->sync([$adminRole->id]);
+
+        $studentRole = Role::query()->where('name', 'Estudiante')->firstOrFail();
+
+        $studentUser = User::factory()->create([
+            'name' => 'estudiante prueba ISW-521',
+            'email' => 'estudiante@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $studentUser->roles()->sync([$studentRole->id]);
+
+        StudentModel::query()->create([
+            'user_id' => $studentUser->id,
+            'national_id' => '0-0000-0000',
+            'name' => 'estudiante',
+            'last_name' => 'prueba',
+            'second_last_name' => 'ISW-521',
+            'active' => true,
+        ]);
+
+        $teachingCoordinatorRole = Role::query()->where('name', 'Coordinadora de Docencia')->firstOrFail();
+
+        $teachingCoordinatorUser = User::factory()->create([
+            'name' => 'docencia prueba ISW-521',
+            'email' => 'docencia@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+        $teachingCoordinatorUser->roles()->sync([$teachingCoordinatorRole->id]);
     }
 }
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -17,5 +17,28 @@ class RoleSeeder extends Seeder
         $superadmin = Role::query()->firstOrCreate(['name' => 'Superadmin']);
         $superadmin->permissions()->sync(Permission::query()->pluck('id'));
         Role::query()->firstOrCreate(['name' => 'Admin']);
+
+        $studentRole = Role::query()->firstOrCreate(['name' => 'Estudiante']);
+        $studentRole->permissions()->sync(
+            Permission::query()->whereIn('name', [
+                'requests.create',
+                'requests.view',
+            ])->pluck('id')
+        );
+
+        $teachingCoordinatorRole = Role::query()->firstOrCreate(['name' => 'Coordinadora de Docencia']);
+        $teachingCoordinatorRole->permissions()->sync(
+            Permission::query()->whereIn('name', [
+                'requests.view',
+                'requests.search',
+                'requests.review',
+                'requests.export_pdf',
+                'requests.export_excel',
+                'waiver_rules.create',
+                'waiver_rules.view',
+                'waiver_rules.edit',
+                'waiver_rules.delete',
+            ])->pluck('id')
+        );
     }
 }
